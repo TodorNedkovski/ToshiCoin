e78a5a772b75cb6ea8cd919d026fe3661f45372a
Todor Nedkovski <tnedkovski@gmail.com>
2021-01-23 13:17:38 +0200

    Cleared Program.cs

--- ToshkoCoin/Program.cs
+++ ToshkoCoin/Program.cs
@@ -9,39 +9,7 @@
     {
         public static void Main()
         {
-            //Blockchain toshiCoin = new Blockchain();
-            //toshiCoin.AddBlock(new Block(null, "{sender:Gosho,receiver:Tosho,amount:10}"));
-            //toshiCoin.AddBlock(new Block(null, "{sender:Valeri,receiver:Tosho,amount:5}"));
-            //toshiCoin.AddBlock(new Block(null, "{sender:Pesho,receiver:Tosho,amount:5}"));
-
-            //Console.WriteLine(toshiCoin.IsValid());
-
-            //toshiCoin.GetLatestBlock().Data = "{sender:Pesho,receiver:Tosho,amount:100000}";
-
-            //Console.WriteLine(toshiCoin.IsValid());
-
-            //toshiCoin.Chain[2].PreviousHash = toshiCoin.Chain[1].Hash;
-            //toshiCoin.Chain[2].Hash = toshiCoin.Chain[2].CalculateHash();
-            //toshiCoin.Chain[3].PreviousHash = toshiCoin.Chain[2].Hash;
-            //toshiCoin.Chain[3].Hash = toshiCoin.Chain[3].CalculateHash();
-
-            //Console.WriteLine(toshiCoin.IsValid());
-
-            //Console.WriteLine(JsonConvert.SerializeObject(toshiCoin, Formatting.Indented));
-
-            var stopwatch = new Stopwatch();
-
-            stopwatch.Start();
-
-            Blockchain toshiCoin = new Blockchain();
-            toshiCoin.PendingTransactions.Add(new Transaction("Gosho", "Tosho", 10));
-            toshiCoin.AddBlock(new Block(null, "{sender:Gosho,receiver:Tosho,amount:10}"));
-            toshiCoin.AddBlock(new Block(null, "{sender:Valeri,receiver:Tosho,amount:5}"));
-            toshiCoin.AddBlock(new Block(null, "{sender:Pesho,receiver:Tosho,amount:5}"));
-
-            stopwatch.Stop();
-
-            Console.WriteLine(stopwatch.Elapsed);
+            //
         }
     }
 }
