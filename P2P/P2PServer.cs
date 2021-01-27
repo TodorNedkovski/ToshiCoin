50a8c917dfc2a4a50c3a975f7ea8d9e34aa7469d
Todor Nedkovski <tnedkovski@gmail.com>
2021-01-27 10:36:47 +0200

    Added p2p server start method

---
+++ P2P/P2PServer.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using WebSocketSharp;
+using WebSocketSharp.Server;
+
+namespace P2PServer
+{
+    public class P2PServer : WebSocketBehavior
+    {
+        bool chainSynched = false;  
+        WebSocketServer wss = null;  
+  
+        public void Start(int port)  
+        {
+            wss = new WebSocketServer($"ws://127.0.0.1:{port}");  
+            wss.AddWebSocketService<P2PServer>("/Blockchain");  
+            wss.Start();  
+            Console.WriteLine($"Started server at ws://127.0.0.1:{port}");  
+        }
+        
+        
+    }
+}
--- P2PServer/P2PServer.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-
-namespace P2PServer
-{
-    public class P2PServer
-    {
-        
-    }
-}
